07974f4c40a8587ec0fd2fda2dff493e9520deb7
Moises Najar <mnajar@gmail.com>
2025-08-28 13:49:26 -0600

    Configura credentials y database.yml para conexión en desarrollo

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Entry point for the build script in your package.json
-console.log('Map:', Map);  // Verificar que Map existe
-console.log('Map.prototype.get:', Map.prototype.get); // Verificar que get existe
+// console.log('Map:', Map);  // Verificar que Map existe
+// console.log('Map.prototype.get:', Map.prototype.get); // Verificar que get existe
 
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
